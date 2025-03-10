8d1e040f59f87ebe5caa5aaed0b91919082877a7
Abhiram <abhiramnksd@gmail.com>
2025-03-10 13:22:31 +0530

    feat:Impliment Middlewere Auth

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,10 +1,11 @@
 const express=require('express')
 const router=express.Router()
 const adminController=require('../controller/adminController')
+const {userAuth,adminAuth}=require('../middlewares/auth')
 
 router.get('/login',adminController.loadlogin)
 router.post('/login',adminController.login)
-router.get('/dashbord',adminController.loadDashbord)
+router.get('/dashbord',adminAuth,adminController.loadDashbord)
 
 
 module.exports=router
